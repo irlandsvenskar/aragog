e604c03bda6992f284bd06471e1395dacb850f08
Clodagh ML <cml_aragog@scamallach.com>
2016-12-11 19:39:49 +0100

    Gets Aragog up and running

--- src/main/java/com/ail/aragog/Aragog.java
+++ src/main/java/com/ail/aragog/Aragog.java
@@ -1,19 +1,18 @@
 package com.ail.aragog;
 
-import com.spotify.apollo.AppInit;
 import com.spotify.apollo.Environment;
 import com.spotify.apollo.httpservice.HttpService;
 import com.spotify.apollo.httpservice.LoadingException;
 import com.spotify.apollo.route.Route;
 
-public final class App {
+public final class Aragog {
 
     public static void main(String... args) throws LoadingException {
-        HttpService.boot(App::init, "aragog", args);
+        HttpService.boot(Aragog::init, "aragog", args);
     }
 
     static void init(Environment environment) {
         environment.routingEngine()
-                .registerAutoRoute(Route.sync("GET", "/", rc -> "Hello world, my name is Aragog"));
+                .registerAutoRoute(Route.sync("GET", "/", rc -> "Hello world, my name is Aragog\n"));
     }
 }
